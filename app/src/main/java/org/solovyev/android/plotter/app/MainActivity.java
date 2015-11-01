1fa823a63eceadf81ce204d1f9fea080190197ea
serso <se.solovyev@gmail.com>
2015-11-01 19:30:41 +0100

    Ui changes

--- app/src/main/java/org/solovyev/android/plotter/app/MainActivity.java
+++ app/src/main/java/org/solovyev/android/plotter/app/MainActivity.java
@@ -9,9 +9,13 @@ import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import org.solovyev.android.plotter.*;
@@ -21,20 +25,19 @@ import javax.annotation.Nonnull;
 
 public class MainActivity extends Activity implements PlotViewFrame.Listener {
 
-	@Bind(R.id.plot_view_frame)
-	PlotViewFrame plotView;
-
 	@Nonnull
 	private final Plotter plotter = PlotterApplication.get().getPlotter();
-
+	@Bind(R.id.plot_view_frame)
+	PlotViewFrame plotView;
 	@Nonnull
 	private final Runnable colorUpdater = new Runnable() {
 		private int direction = -1;
+
 		@Override
 		public void run() {
 			final PlotData plotData = plotter.getPlotData();
 			final PlotFunction function = plotData.get(PlotterApplication.PARABOLOID);
-			if(function == null) {
+			if (function == null) {
 				return;
 			}
 			final Color color = function.meshSpec.color;
@@ -100,10 +103,13 @@ public class MainActivity extends Activity implements PlotViewFrame.Listener {
 		return false;
 	}
 
-	public class DimensionsDialog {
+	public class DimensionsDialog implements TextView.OnEditorActionListener {
 
 		@Nonnull
-		protected final View view;
+		private final View view;
+		@Nonnull
+		private final AlertDialog dialog;
+		private final boolean d3;
 		@Bind(R.id.x_min_edittext)
 		EditText xMin;
 		@Bind(R.id.x_max_edittext)
@@ -114,9 +120,8 @@ public class MainActivity extends Activity implements PlotViewFrame.Listener {
 		EditText yMax;
 		@Bind(R.id.y_bounds)
 		View yBounds;
-		private final boolean d3;
 
-		protected DimensionsDialog(@Nonnull Context context, @Nonnull RectF graph, boolean d3) {
+		protected DimensionsDialog(@Nonnull Context context, @Nonnull RectF graph, final boolean d3) {
 			this.d3 = d3;
 			view = LayoutInflater.from(context).inflate(R.layout.dialog_dimensions, null);
 			ButterKnife.bind(this, view);
@@ -128,33 +133,47 @@ public class MainActivity extends Activity implements PlotViewFrame.Listener {
 			if (d3) {
 				yBounds.setVisibility(View.GONE);
 			}
-		}
 
-		private void setDimension(@Nonnull EditText view, float value) {
-			view.setText(String.format("%.2f", value));
-		}
-
-		public void show() {
 			final AlertDialog.Builder b = new AlertDialog.Builder(view.getContext());
 			b.setView(view);
 			b.setCancelable(true);
 			b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					final RectF graph = new RectF(getDimension(xMin), getDimension(yMin), getDimension(xMax), getDimension(yMax));
-					if (graph.isEmpty()) {
-						new DimensionsDialog(MainActivity.this, graph, d3).show();
-						return;
-					}
-					if (!d3) {
-						plotter.updateGraph(null, new RectSizeF(graph.width(), graph.height()), new PointF(graph.centerX(), graph.centerY()));
-					} else {
-						final Dimensions dimensions = plotter.getDimensions();
-						plotter.updateGraph(null, new RectSizeF(graph.width(), dimensions.graph.height()), new PointF(graph.centerX(), dimensions.graph.center.y));
-					}
+					close(d3);
 				}
 			});
-			b.show();
+			dialog = b.create();
+			dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+				@Override
+				public void onShow(DialogInterface dialog) {
+					final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.showSoftInput(xMin, InputMethodManager.SHOW_IMPLICIT);
+				}
+			});
+		}
+
+		private void close(boolean d3) {
+			final RectF graph = new RectF(getDimension(xMin), getDimension(yMin), getDimension(xMax), getDimension(yMax));
+			if (graph.isEmpty()) {
+				new DimensionsDialog(MainActivity.this, graph, d3).show();
+				return;
+			}
+			if (!d3) {
+				plotter.updateGraph(null, new RectSizeF(graph.width(), graph.height()), new PointF(graph.centerX(), graph.centerY()));
+			} else {
+				final Dimensions dimensions = plotter.getDimensions();
+				plotter.updateGraph(null, new RectSizeF(graph.width(), dimensions.graph.height()), new PointF(graph.centerX(), dimensions.graph.center.y));
+			}
+		}
+
+		private void setDimension(@Nonnull EditText view, float value) {
+			view.setOnEditorActionListener(this);
+			view.setText(String.format("%.2f", value));
+		}
+
+		public void show() {
+			dialog.show();
 		}
 
 		private float getDimension(@Nonnull EditText view) {
@@ -164,6 +183,16 @@ public class MainActivity extends Activity implements PlotViewFrame.Listener {
 				Log.e(Plot.getTag("MainActivity"), e.getMessage(), e);
 				return 0f;
 			}
+		}
+
+		@Override
+		public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+			if (actionId == EditorInfo.IME_ACTION_DONE) {
+				dialog.dismiss();
+				close(d3);
+				return true;
+			}
+			return false;
 		}
 	}
 }
