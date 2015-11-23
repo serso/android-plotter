2afb06f1181acea48d002d4f6e9dce0ccb8ea146
serso <se.solovyev@gmail.com>
2015-11-23 14:40:27 +0100

    exp4j library update

--- app/src/main/java/org/solovyev/android/plotter/math/ExpressionFunction.java
+++ app/src/main/java/org/solovyev/android/plotter/math/ExpressionFunction.java
@@ -51,14 +51,14 @@ public final class ExpressionFunction extends Function {
 
     @Override
     public float evaluate(float x) {
-        expression.setVariable(variables[0], (double) x);
+        expression.setVariable(variables[0], x);
         return (float) expression.evaluate();
     }
 
     @Override
     public float evaluate(float x, float y) {
-        expression.setVariable(variables[0], (double) x);
-        expression.setVariable(variables[1], (double) y);
+        expression.setVariable(variables[0], x);
+        expression.setVariable(variables[1], y);
         return (float) expression.evaluate();
     }
 
